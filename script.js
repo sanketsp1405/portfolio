8974850f3fa58020cda2b63c5cd1a92fc0ff2b1a
sanketsp1405 <155372431+sanketsp1405@users.noreply.github.com>
2025-05-21 12:52:48 +0530

    Update script.js
    new changes

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-// Add your JavaScript here if needed
-console.log("Portfolio Loaded");
-document.querySelectorAll('.flip-card').forEach(card => {
-    card.addEventListener('click', () => {
-      card.classList.toggle('flipped');
-    });
-  });
-  function toggleEduCard(box) {
+// Toggle education details on click
+const boxes = document.querySelectorAll('.edu-box');
+boxes.forEach(box => {
+  box.addEventListener('click', () => {
     box.classList.toggle('active');
-}
+  });
+});
